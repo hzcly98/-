285e91a7204deab30dac18b6503e06f67c5e7ff2
利民网 <317900981@qq.com>
2017-07-12 15:56:42 +0800

    第一次上传

--- 串行 并行 同步 异步/ViewController.m
+++ 串行 并行 同步 异步/ViewController.m
@@ -16,14 +16,83 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 串行 + 同步
+- (IBAction)serialSyncClick:(id)sender {
+    NSLog(@"start log in main thread");
+    dispatch_queue_t myQueue = dispatch_queue_create("myQueue", NULL);
+    for (NSInteger n = 0; n < 3; n++) {
+        dispatch_sync(myQueue, ^{
+            for (NSInteger i = 0; i < 500000000; i++) {
+                if (i == 0) {
+                    NSLog(@"串行同步任务%ld -> 开始%@",n,[NSThread currentThread]);
+                }
+                if (i == 499999999) {
+                    NSLog(@"串行同步任务%ld -> 完成",(long)n);
+                }
+            }
+        });
+    }
+    NSLog(@"阻塞我没有？当前线程%@",[NSThread currentThread]);
 }
 
+// 串行 + 异步
+- (IBAction)serialAsyncClick:(id)sender {
+    NSLog(@"start log in main thread");
+    dispatch_queue_t myQueue = dispatch_queue_create("myQueue", NULL);//创建一个串行队列
+    for (NSInteger n = 0; n < 3; n++) {
+        dispatch_async(myQueue, ^{
+            for (NSInteger i = 0; i < 500000000; i++) {
+                if (i == 0) {
+                    NSLog(@"串行异步任务%ld -> 开始%@",n,[NSThread currentThread]);
+                }
+                if (i == 499999999) {
+                    NSLog(@"串行异步任务%ld -> 完成",(long)n);
+                }
+            }
+        });
+    }
+    NSLog(@"阻塞我没有？当前线程%@",[NSThread currentThread]);
+}
+
+// 并行 + 同步
+- (IBAction)concurrentSyncClick:(id)sender {
+    NSLog(@"start log in main thread");
+    dispatch_queue_t myQueue = dispatch_queue_create("myQueue", DISPATCH_QUEUE_CONCURRENT);
+    for (NSInteger n = 0; n < 3; n++) {
+        dispatch_sync(myQueue, ^{
+            for (NSInteger i = 0; i < 500000000; i++) {
+                if (i == 0) {
+                    NSLog(@"并行同步任务%ld -> 开始%@",(long)n,[NSThread currentThread]);
+                }
+                if (i == 499999999) {
+                    NSLog(@"并行同步任务%ld -> 完成",(long)n);
+                }
+            }
+        });
+    }
+    
+    NSLog(@"阻塞我没有？当前线程%@",[NSThread currentThread]);
+}
+
+// 并行 + 异步
+- (IBAction)concurrentAsyncClick:(id)sender {
+    NSLog(@"start log in main thread");
+    dispatch_queue_t myQueue = dispatch_queue_create("myQueue", DISPATCH_QUEUE_CONCURRENT);
+    for (NSInteger n = 0; n < 3; n++) {
+        dispatch_async(myQueue, ^{
+            for (NSInteger i = 0; i < 500000000; i++) {
+                if (i == 0) {
+                    NSLog(@"并行异步任务%ld -> 开始%@",n,[NSThread currentThread]);
+                }
+                if (i == 499999999) {
+                    NSLog(@"并行异步任务%ld -> 完成",(long)n);
+                }
+            }
+        });
+    }
+    NSLog(@"阻塞我没有？当前线程%@",[NSThread currentThread]);
+}
 
 @end
